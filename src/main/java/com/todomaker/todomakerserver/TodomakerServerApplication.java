6bd3e0c49f7a634dacd940ab90dc928b36f478bc
ielay <ielay@yandex.ru>
2019-07-20 21:22:14 +0300

    Created maven project structure with spring-boot-starter

---
+++ src/main/java/com/todomaker/todomakerserver/TodomakerServerApplication.java
@@ -0,0 +1,13 @@
+package com.todomaker.todomakerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodomakerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodomakerServerApplication.class, args);
+    }
+
+}
