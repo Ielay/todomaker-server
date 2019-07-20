3f6eba13d542aaaa9e8da8726e7f133110295c05
ielay <ielay@yandex.ru>
2019-07-20 21:31:52 +0300

    + Renamed main class

--- src/main/java/com/todomaker/todomakerserver/TodoMakerServerApplication.java
+++ src/main/java/com/todomaker/todomakerserver/TodoMakerServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TodomakerServerApplication {
+public class TodoMakerServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TodomakerServerApplication.class, args);
+        SpringApplication.run(TodoMakerServerApplication.class, args);
     }
 
 }
--- src/test/java/com/todomaker/todomakerserver/TodoMakerServerApplicationTests.java
+++ src/test/java/com/todomaker/todomakerserver/TodoMakerServerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TodomakerServerApplicationTests {
+public class TodoMakerServerApplicationTests {
 
     @Test
     public void contextLoads() {
